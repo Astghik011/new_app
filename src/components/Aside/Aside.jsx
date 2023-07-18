773cf711757b90cab4e132292b4f04685f183544
Astghik Simonyan <astghiksimonyan56@gmail.com>
2023-07-18 23:42:17 +0400

    adding main and aside

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,8 +1,58 @@
-// import styles from './Aside.module.css'
+import styles from './Aside.module.css'
 
 function Aside(){
-    return <>
-    </>
+    return <div>
+        <aside>
+            <figure className="order">
+                <h2>Корзина</h2>
+                <span id="number_of_orders">4</span>
+            </figure>
+        <div class="orders">
+            <figure class="order">
+                <img class="thmb thmb1" src="thumbnails/Rectangle1.png" />
+                <figcaption class="order_details">
+                    <h3 class="order_name">Супер сырный</h3>
+                    <p class="weight">512г</p>
+                    <p class="price">550₽</p>
+                </figcaption>
+                <div class="add_order">
+                    <button class="subtract">-</button><input class="number" value = "0" /><button class="add">+</button>             
+                </div>
+            </figure>
+            <figure class="order">
+                <img class="thmb thmb2" src="thumbnails/Rectangle2.png" />
+                <figcaption class="order_details">
+                    <h3 class="order_name">Картошка фри</h3>
+                    <p class="weight">180г</p>
+                    <p class="price">245₽</p>
+                </figcaption>
+                <div class="add_order">
+                    <button class="subtract">-</button><input class="number" value = "0" /><button class="add">+</button>
+                </div>
+            </figure>
+            <figure class="order">
+                <img class="thmb thmb3" src="thumbnails/Rectangle3.png" />
+                <figcaption class="order_details">
+                    <h3 class="order_name">Жгучий хот-дог</h3>
+                    <p class="weight">245г</p>
+                    <p class="price">239₽</p>
+                </figcaption>
+                <div class="add_order">
+                    <button class="subtract">-</button><input type="text" class="number" value = "0" /><button class="add">+</button>        
+                </div>
+            </figure>
+            <div class="order total">
+                <span class="sum_total">Итого</span>
+                <span id="total_price">1235₽</span>
+            </div>
+                <button onclick="showDeliveryWindow()" type="button" id="submit_order">Оформить заказ</button>
+            <div class="free_delivery">
+                <img id="delivery" src="icons/free-icon-delivery-2362252.png" />
+                <span>Бесплатная доставка</span>
+            </div>
+        </div>
+        </aside>
+    </div>
 }
 
 export default Aside
--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,5 +1,67 @@
+import styles from './MainContent.module.css'
+import burger1 from '../../images/burger_images/burger_1.jpg'
+import burger2 from '../../images/burger_images/burger_2.jpg'
+import burger3 from '../../images/burger_images/burger_3.jpg'
+import burger4 from '../../images/burger_images/burger_4.jpg'
+import burger5 from '../../images/burger_images/burger_5.jpg'
+import burger6 from '../../images/burger_images/burger_6.jpg'
+
+
 function MainContent(){
-    return <></>
+    // let response = fetch('https://jsonplaceholder.typicode.com/posts')
+    //     .then((response) => response.json())
+    //     .then((data) => console.log(data))
+    // let titles = response.map((data) => data.title)
+    // return (<div>
+    //     {titles}
+    // </div>)
+    return (
+        <div className={`${styles.container} ${styles.main_content}`}>
+        <main>
+            <div className={styles.products_main}>
+                <img className={styles.productsImg} src={burger1} alt="Burger" />
+                <span className={styles.price_main}>640₽</span>
+                <p className={styles.productsName}>Филадельфия</p>
+                <span className={styles.productsMass}>520г</span>
+                <button className={styles.btn_main}>Добавить</button>
+            </div>
+            <div className={styles.products_main}>
+                <img className={styles.productsImg} src={burger2} alt="Burger" />
+                <span className={styles.price_main}>687₽</span>
+                <p className={styles.productsName}>Королевский</p>
+                <span className={styles.productsMass}>500г</span>
+                <button className={styles.btn_main}>Добавить</button>
+            </div>
+            <div className={styles.products_main}>
+                <img className={styles.productsImg} src={burger3} alt="Burger" />
+                <span className={styles.price_main}>590₽</span>
+                <p className={styles.productsName}>Гавайский лосось</p>
+                <span className={styles.productsMass}>450г</span>
+                <button className={styles.btn_main}>Добавить</button>
+            </div>
+            <div className={styles.products_main}>
+                <img className={styles.productsImg} src={burger4} alt="Burger" />
+                <span className={styles.price_main}>570₽</span>
+                <p className={styles.productsName}>Кубанский</p>
+                <span className={styles.productsMass}>480г</span>
+                <button className={styles.btn_main}>Добавить</button>
+            </div>
+            <div className={styles.products_main}>
+                <img className={styles.productsImg} src={burger5} alt="Burger" />
+                <span className={styles.price_main}>725₽</span>
+                <p className={styles.productsName}>Белый дракон</p>
+                <span className={styles.productsMass}>510г</span>
+                <button className={styles.btn_main}>Добавить</button>
+            </div>
+            <div className={styles.products_main}>
+                <img className={styles.productsImg} src={burger6} alt="Burger" />
+                <span className={styles.price_main}>620₽</span>
+                <p className={styles.productsName}>Мексиканский</p>
+                <span className={styles.productsMass}>520г</span>
+                <button className={styles.btn_main}>Добавить</button>
+            </div>
+        </main>
+    </div>)
 }
 
 export default MainContent
