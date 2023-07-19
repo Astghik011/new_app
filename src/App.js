a65d0774cecd85ebeef0b3366b32495ddc8f00fd
Astghik Simonyan <astghiksimonyan56@gmail.com>
2023-07-19 21:35:43 +0400

    adding delivery window

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Header from './components/Header/Header'
 import Aside from './components/Aside/Aside'
 import './App.css'
 import {lazy, Suspense} from "react";
+import DeliveryWindow from './components/DeliveryWindow/DeliveryWindow'
 
 const MainContent = lazy(()=>import("./components/MainContent/MainContent"))
 
 function App(){
   return <>
     <Header />
+    <DeliveryWindow />
     <NavBar />
     <Aside />
     <Suspense fallback ={<div style = {{width: "200px", margin: "200px auto"}}>Loading...</div>}>
